5c190833151e836c33461d15213dd7be275f7e11
archercreat <archercreat@gmail.com>
2023-05-03 22:46:29 +0000

    update

--- default.nix
+++ default.nix
@@ -1,7 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
 {
-  overlays = import ./overlays;
-
   z3     = pkgs.callPackage ./pkgs/z3 {};
   xed    = pkgs.callPackage ./pkgs/xed {};
   triton = pkgs.callPackage ./pkgs/triton {};
